5fde7caecc759865951eea98cfbef19a20d777d6
Bob Rockers <bob@rockerssoft.org>
2015-09-29 14:55:33 -0500

    Merged moser branch into master.

--- js/application.js
+++ js/application.js
@@ -1,1 +1,43 @@
-/* Javascript */ 
+/* Javascript */
+$(document).ready(function() {
+	// Obtain a reference to the canvas element
+	// using its id.
+	var htmlCanvas = document.getElementById('myCanvas');
+
+	// Obtain a graphics context on the
+	// canvas element for drawing.
+	var context = htmlCanvas.getContext('2d');
+
+	// Start listening to resize events and
+	// draw canvas.
+	initialize();
+
+	function initialize() {
+		// Register an event listener to
+		// call the resizeCanvas() function each time
+		// the window is resized.
+		window.addEventListener('resize', resizeCanvas, false);
+
+		// Draw canvas border for the first time.
+		resizeCanvas();
+	}
+
+	// Display custom canvas.
+	// In this case it's a blue, 5 pixel border that
+	// resizes along with the browser window.
+	function redraw() {
+		context.strokeStyle = 'blue';
+		context.lineWidth = '5';
+		context.strokeRect(0, 0, window.innerWidth, window.innerHeight);
+	}
+
+	// Runs each time the DOM window resize event fires.
+	// Resets the canvas dimensions to match window,
+	// then draws the new borders accordingly.
+	function resizeCanvas() {
+		htmlCanvas.width = window.innerWidth;
+		htmlCanvas.height = window.innerHeight;
+		redraw();
+	}
+
+});
