4a6cf8dd0976cb11d14393bebffd4e1ea256b9ff
Carson McKinstry <carson.mckinstry@gmail.com>
2017-04-04 20:45:35 -0500

    Changed angle to ouput as deg/min/sec

--- js/application.js
+++ js/application.js
@@ -108,7 +108,11 @@ $(document).ready(function() {
 			var AB = Math.sqrt(Math.pow(B.x-A.x,2)+ Math.pow(B.y-A.y,2));
 			var BC = Math.sqrt(Math.pow(B.x-C.x,2)+ Math.pow(B.y-C.y,2));
 			var AC = Math.sqrt(Math.pow(C.x-A.x,2)+ Math.pow(C.y-A.y,2));
-			angDisplay.innerHTML = radianToDegrees( Math.acos((BC*BC+AB*AB-AC*AC)/(2*BC*AB)) );
+
+			let angle = convertDDToDMS(radianToDegrees( Math.acos((BC*BC+AB*AB-AC*AC)/(2*BC*AB)) ))
+			// console.log(angle);
+
+			angDisplay.innerHTML = `${angle.deg}&deg; ${angle.minutes}' ${angle.seconds}"`
 		} else {
 			angDisplay.innerHTML = "N/A";
 		}
@@ -305,6 +309,6 @@ $(document).ready(function() {
 	}
 
 	// draw canvas.
-	// initialize();
+	initialize();
 
 });
